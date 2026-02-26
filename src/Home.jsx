61b3a8c729ddd8584fc628a5625b73f23a3e8828
Ema <240557@utxicotepec.edu.mx>
2026-02-26 09:49:17 -0600

    hshsh

---
+++ src/Home.jsx
@@ -0,0 +1,17 @@
+import "./Home.css";
+
+function Home() {
+  return (
+    <section className="hero">
+      <div className="hero-content">
+        <h1>
+          <span className="white">PAOLO</span>{" "}
+          <span className="yellow">MALDINI</span>
+        </h1>
+        <p>Leyenda eterna del fútbol europeo</p>
+      </div>
+    </section>
+  );
+}
+
+export default Home;
